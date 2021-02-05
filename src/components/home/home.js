57f6e7fe9d7e0e9e7756783eeae93981d044fb93
VitorPiovezan <vitor.piovezan@hotmail.com.br>
2021-02-05 04:01:46 -0300

    Component Contato criado

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,72 +1,184 @@
+import React, {useEffect, useState} from 'react';
+
 import banner01 from '../../assets/banners/banner-site.jpg';
 import logo01 from '../../assets/logos/logo.png';
 import face from '../../assets/logos/face.png';
 import insta from '../../assets/logos/insta.png';
 import textimg1 from '../../assets/images/Ativo2bigode.png';
 import produto from '../../assets/images/Lata_exemplo.png';
+import dogcell from '../../assets/images/cachorrocelular.png';
+import whats from '../../assets/logos/whats.png'
+import pata from '../../assets/images/fg.png';
 
 import {
-    Menu,
-    Banner,
-    Imagem,
-    Logo,
-    Sobre,
-    TituloSobre,
-    Nav,
-    NavTab,
-    NavLi,
-    ContatoTab,
-    ContatoLi,
-    RedeSocial,
-    Link,
-    ImagemText,
-    DadosDieta,
-    DadosProduct,
-    TextProduct,
-    ProductImagem,
-    ButtonProdut,
-    TextButton
+  Menu,
+  Banner,
+  Imagem,
+  Logo,
+  Sobre,
+  TituloSobre,
+  Nav,
+  NavTab,
+  NavLi,
+  ContatoTab,
+  ContatoLi,
+  RedeSocial,
+  Link,
+  ImagemText,
+  DadosDieta,
+  DadosProduct,
+  TextProduct,
+  ProductImagem,
+  ButtonProdut,
+  TextButton,
+  Contato,
+  FormContato,
+  TitleContato,
+  Img1,
+  ImputNome,
+  ButtomWhats,
+  ImgBackground,
+  ButtomFace,
+  ContatoImagem,
+  BackgroundContato,
+  TextBox,
+  // Footer,
+  // Atendimento,
+  // TitleFooter,
+  // TextFooter,
+  // RedesSociais,
+  // ButtomFacebook,
+  // ButtomInsta,
+  // ImgBackgroundFooter,
+  // Mapa,
+  Container
+  
+
 } from './styles'
 
-function Home() {
-  return (
-    <div>
-      <header>
 
-        <Menu>
+
+function Home() {
+
+  const [widthWindow, setWidthWindow] = useState(window.screen.availWidth);
+  const [linkWhats, setLinkWhats] = useState("");
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+  useEffect(() => {
+    console.log(nome)
+    console.log(email)
+    console.log(mensagem)
+    setLinkWhats("https://api.whatsapp.com/send?phone=5516996163774&text=*Nome:*%20"+nome+"%0a*Email:*%20"+email+"%0a%0a"+mensagem)
+    console.log(linkWhats)
+  }, [email, nome, mensagem]);
+
+  if (window.screen.width <= 500){
+    return (
+      <div>
+        <Contato>
+            <FormContato>
+              <TitleContato><Img1 src={textimg1}/>Entre em Contato<Img1 src={textimg1}/></TitleContato>
+                <ImputNome placeholder="Seu Nome" type="text"
+                                                  value={nome}
+                                                  onChange={e => setNome(e.target.value)}></ImputNome>
+                <ImputNome placeholder="Seu Email"type="text"
+                                                  value={email}
+                                                  onChange={e => setEmail(e.target.value)}></ImputNome>
+                <TextBox placeholder="Sua Mensagem"type="text"
+                                                  value={mensagem}
+                                                  onChange={e => setMensagem(e.target.value)}></TextBox>
+                <ButtomWhats><a href={linkWhats} target="blank"><ImgBackground src={whats}/></a></ButtomWhats>
+                <ButtomFace><ImgBackground /></ButtomFace>
+            </FormContato>
+                <ContatoImagem>
+                  <BackgroundContato src={dogcell}/>
+                </ContatoImagem>
+          </Contato>
+      </div>
+      )
+  } else {
+    return (
+      <div>
+        <header>
+  
+          <Menu>
             <Nav>
-                <NavTab>
-                    <NavLi><Link href="https://agenciaolcan.com.br">Sobre</Link></NavLi>
-                    <NavLi>Produtos</NavLi>
-                    <NavLi>Contatos</NavLi>
-                </NavTab>
-                <ContatoTab>
-                    <ContatoLi><RedeSocial src={face}/></ContatoLi>
-                    <ContatoLi><RedeSocial src={insta}/></ContatoLi>
-                </ContatoTab>
+              <NavTab>
+                <NavLi><Link href="https://agenciaolcan.com.br">Sobre</Link></NavLi>
+                <NavLi>Produtos</NavLi>
+                <NavLi>Contatos</NavLi>
+              </NavTab>
+              <ContatoTab>
+                <ContatoLi><RedeSocial src={face} /></ContatoLi>
+                <ContatoLi><RedeSocial src={insta} /></ContatoLi>
+              </ContatoTab>
             </Nav>
-            <Logo src={logo01}/>
-        </Menu>
-        <Banner>
-            <Imagem src={banner01}/>
-        </Banner>
-      </header>
-      <main>
-        <Sobre>
-            <TituloSobre>Dieta Personalizada <ImagemText src={textimg1}/> </TituloSobre>
+            <Logo src={logo01} />
+          </Menu>
+          <Banner>
+            <Imagem src={banner01} />
+          </Banner>
+        </header>
+        <Container>
+          <Sobre>
+            <TituloSobre>Dieta Personalizada <ImagemText src={textimg1} /> </TituloSobre>
             <DadosDieta>
-                <ProductImagem src={produto}/>
-                <DadosProduct>
-                  <TextProduct>Nossa receita de sucesso é preparar comidas deliciosas. saudáveis e nutritivas para os amiguinhos de quatro patas! Tudo fresquinho sem conservantes, feito de forma caseira. É assim que Sol Pet Food trabalha.</TextProduct>
-                  <ButtonProdut>
-                    <TextButton>Preencha Já</TextButton>
-                  </ButtonProdut>
-                </DadosProduct>
+              <ProductImagem src={produto} />
+              <DadosProduct>
+                <TextProduct>Nossa receita de sucesso é preparar comidas deliciosas. saudáveis e nutritivas para os amiguinhos de quatro patas! Tudo fresquinho sem conservantes, feito de forma caseira. É assim que Sol Pet Food trabalha.</TextProduct>
+                <a href="https://agenciaolcan.com.br"><ButtonProdut>
+                  <TextButton>Preencha Já</TextButton>
+                </ButtonProdut></a>
+              </DadosProduct>
             </DadosDieta>
-        </Sobre>
-      </main>
-    </div>
-  );
+          </Sobre>
+  
+          <Contato>
+            <FormContato>
+              <TitleContato><Img1 src={pata}/>Entre em Contato<Img1 width="50px" src={textimg1}/></TitleContato>
+                <ImputNome placeholder="Seu Nome" type="text"
+                                                  value={nome}
+                                                  onChange={e => setNome(e.target.value)}></ImputNome>
+                <ImputNome placeholder="Seu Email"type="text"
+                                                  value={email}
+                                                  onChange={e => setEmail(e.target.value)}></ImputNome>
+                <TextBox placeholder="Sua Mensagem"type="text"
+                                                  value={mensagem}
+                                                  onChange={e => setMensagem(e.target.value)}></TextBox>
+                <ButtomWhats><a href={linkWhats} target="blank"><ImgBackground src={whats}/></a></ButtomWhats>
+                <ButtomFace><ImgBackground /></ButtomFace>
+            </FormContato>
+                <ContatoImagem>
+                  <BackgroundContato src={dogcell}/>
+                </ContatoImagem>
+          </Contato>
+  
+              {/* <Footer>
+                <Atendimento>
+                  <TitleFooter>Atendimento</TitleFooter>
+                  <TextFooter><B>Email: </B>solpetfood@gmail.com</TextFooter>
+                  <TextFooter><B>Facebook: </B>/SolPetFood</TextFooter>
+                  <TextFooter><B>Whats: </B>(16) 99631-2366</TextFooter>
+                </Atendimento>
+  
+                <RedesSociais>
+                  <TitleFooter>Redes Socias</TitleFooter>
+                  <ButtomFacebook><ImgBackgroundFooter /></ButtomFacebook>
+                  <ButtomInsta><ImgBackgroundFooter /></ButtomInsta>
+                </RedesSociais>
+  
+                <Mapa>
+                  <TitleFooter>Mapa</TitleFooter>
+                </Mapa>
+              </Footer> */}
+        </Container>
+      </div>
+    );
+  }
+
+  
 }
 
 export default Home;
--- src/components/home/styles.js
+++ src/components/home/styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+    flex-direction: column;
+    margin: auto;
+    display: flex;
+    width: 70%;
+`
+
 export const Teste = styled.h1`
     padding: 7px;
     color: #4484CE;
@@ -133,3 +140,127 @@ export const TextButton = styled.p`
 
 //Depoimentos
 
+//Contato
+
+
+export const Contato = styled.div`
+    width: 100%;
+    border: 0.2px dotted #282828;
+    margin-bottom: 50px;  
+    text-align: center;
+    height: 450px;
+    display: flex;
+    flex-direction: row;
+`
+
+
+export const TitleContato = styled.h1`
+    font-weight: bold;
+    font-family: 'Ubuntu';
+    font-size: 25pt;
+    color: #282828;
+    padding: 10px;
+    margin-bottom: 20px;
+`
+
+export const Img1 = styled.img`
+    height: 25px;
+    padding: 0px 15px;
+    transform: rotate(10deg);
+`
+
+export const ImputNome = styled.input`
+    margin-top: 10px;
+    padding: 5px;
+    width: 80%;
+    border: 0 none;
+    border-bottom: 1px dotted #282828;
+    transition: 0.4s;
+    padding: 10px;
+    font-size: 12pt;
+
+    &:focus {  
+    box-shadow: 0 0 0 0;
+    outline: 0;
+
+    }
+`
+
+export const TextBox = styled.textarea`
+    margin-top: 10px;
+    padding: 5px;
+    width: 80%;
+    height: 40%;
+    border: 0 none;
+    border-bottom: 1px dotted #282828;
+    transition: 0.4s;
+    padding: 10px;
+    font-size: 12pt;
+    margin-top: 40px;
+    vertical-align: text-bottom;
+    margin-bottom: 10px;
+
+    &:focus {  
+    box-shadow: 0 0 0 0;
+    outline: 0;
+
+    }
+`
+
+export const FormContato = styled.div`
+    margin-top: 40px;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 85%;
+`
+
+
+export const ButtomWhats = styled.div`
+    width: 45px;
+    margin-left: -75%;
+    transition: 0.4s;
+
+    &:hover {  
+    width: 50px;
+    }
+`
+
+export const ImgBackground = styled.img`
+    width: 45px;
+    transition: 0.4s;
+
+    &:hover {  
+        width: 50px;
+    }
+`
+
+export const ButtomFace = styled.div`
+`
+
+export const ContatoImagem = styled.div`
+    max-width: 50%;
+    opacity: 80%;
+    display: flex;
+`
+
+export const BackgroundContato = styled.img`
+    height: 100%;
+    width: 100%;
+    -moz-transform: scaleX(-1);
+    -o-transform: scaleX(-1);
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+`
+//Footer
+
+// Footer,
+// Atendimento,
+// TitleFooter,
+// TextFooter,
+// RedesSociais,
+// ButtomFacebook,
+// ButtomInsta,
+// ImgBackgroundFooter,
+// Mapa,
